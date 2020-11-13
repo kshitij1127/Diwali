38b923d786c44515d482c8ff73ca5bf94d361379
kshitij <63649035+kshitij1127@users.noreply.github.com>
2020-11-13 08:02:25 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -3,7 +3,7 @@
   var gravity;
   
   function setup() {
-    createCanvas(600, 600);
+    createCanvas(windowWidth , windowHeight);
     colorMode(HSB);
     gravity = createVector(0, 0.2);
     stroke(255);
@@ -14,6 +14,9 @@
   function draw() {
     colorMode(RGB);
     background(0, 0, 0, 25);
+
+    textSize(35);
+    text("Happy Diwali !",width/2,height/4);
   
     if (random(1) < 0.03) {
       fireworks.push(new Firework());
